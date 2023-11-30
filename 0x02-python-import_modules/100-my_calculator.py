b21e26323df67cf116771dc3455d0fd30016e4c9
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-11-30 08:11:15 -0800

    python is fun

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
 
     if sys.argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == "_":
+    elif sys.argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif sys.argv[2] == "*":
         print("{} * {} = {}"'.format.(a, b, mul(a, b)))
     elif sys.argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("unknown operator. Available operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)

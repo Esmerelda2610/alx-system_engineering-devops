7948cf1d3f48820a1a9921a2a1e48cb69ca773a3
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2024-01-08 19:03:47 -0800

    python is fun

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,8 @@
 class MyList(list):
     ''' Represents a MyList
     '''
-
-   def print_sorted(self):
-       '''
-       prints the list, but sorted
-       '''
-       print(sorted(self))
+    def print_sorted(self):
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))

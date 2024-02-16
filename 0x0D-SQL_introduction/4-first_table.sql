460fac2c45827775f0be5b2baec37cc85c31c10c
Esmerelda2610 <Esmerelda2610@github.com>
2024-02-15 16:03:39 -0800

    SQL Introductions

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
---Write a script that creates a table called first_table in the current db
-CREATE table IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
+-- Create a table 'first_table' in current database
+-- in MySQL server
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Prints full description of first_table
-SHOW CREATE table first_tablei;
+SHOW CREATE TABLE first_table;
